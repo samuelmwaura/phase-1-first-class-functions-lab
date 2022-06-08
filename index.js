74514b508afe70f38e63921d52d2987a325c3b3b
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-06-08 23:55:25 +0300

    Completed the Lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = arrayOfDrivers=> arrayOfDrivers.slice(0,2);// arrow functions have implicit return values.
+
+const returnLastTwoDrivers = (arrayOfDrivers)=> arrayOfDrivers.slice(-2);
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+function createFareMultiplier(integer){
+    return (fare)=>integer*fare;
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = (arrayOfDrivers,callbackfunction)=>{// passed by reference
+   return callbackfunction(arrayOfDrivers); // called here
+}
